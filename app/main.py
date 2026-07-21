e162fbbb2e850b1f9f0af37056cf37b55f54130b
mazranorroz-prog <mazranorroz@gmail.com>
2026-07-21 11:49:42 -0400

    Rebuild FastAPI project structure

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="SneakerHunter API",
+    version="1.0"
+)
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "SneakerHunter API running"
+    }
