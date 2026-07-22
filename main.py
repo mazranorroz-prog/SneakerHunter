710c7fad1a3b7e3d563494dc10ff2634991daea6
mazranorroz-prog <mazranorroz@gmail.com>
2026-07-22 07:48:33 -0400

    Milestone 1 - FastAPI, PostgreSQL, SQLAlchemy, Alembic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 
-from app.database.connection import engine
-
 app = FastAPI(
     title="SneakerHunter API",
     version="1.0.0"
@@ -11,6 +9,5 @@ app = FastAPI(
 @app.get("/")
 def home():
     return {
-        "message": "SneakerHunter API is running",
-        "database": engine.url.render_as_string(hide_password=True)
+        "message": "SneakerHunter API is running"
     }
